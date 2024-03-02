97a894585bdb2f0987074f1df7b92fb2f98e6a56
jeelgandhi123 <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-02 22:31:00 +0530

    Create loopinc.c

---
+++ loopinc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+ //for loop
+ for(int i=0;i<2;i++){
+    printf("hello::for loop\n");
+ }
+
+ //while loop
+ int i=0;
+ while(i<2){
+    printf("hello::while loop\n");
+    i++;
+ }
+
+ //do while loop
+ i=0;
+ do{
+    printf("hello::do while \n");
+    i++;
+ }while(i<2);
+}
+
